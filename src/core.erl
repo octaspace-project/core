9c457e5d84b8343d60829ca39257aa286dd20e8c
x9q <x9q@x9q.com>
2020-05-10 00:15:28 +0300

    Add core:env/{1,2}

--- src/core.erl
+++ src/core.erl
@@ -1,8 +1,16 @@
 -module(core).
 
 -export([uptime/0]).
+-export([env/1]).
+-export([env/2]).
 
 -spec uptime() -> non_neg_integer().
 uptime() ->
     {T, _} = erlang:statistics(wall_clock),
     T div 1000.
+
+env(Option) ->
+    env(Option, undefined).
+
+env(Option, Default) ->
+    application:get_env(application:get_env(core, app, core), Option, Default).
